8a5e528f06b5cc9a9b96561ed5854c1580cfac6d
aiacuone <aiacuone@users.noreply.github.com>
2021-09-07 18:51:12 +0100

    stores phone screen basics complete

--- components/phone/PhoneScreen.js
+++ components/phone/PhoneScreen.js
@@ -8,12 +8,13 @@ import headerBackground from '../../public/images/headerBackground.svg'
 import Image from 'next/image'
 import IconButton from '@material-ui/core/IconButton'
 import Drawer from '@material-ui/core/Drawer'
-// import Slider from './Slider'
+import Typography from '@material-ui/core/Typography'
 import Sliders from './Sliders'
 import Button from '@material-ui/core/Button'
+import Radio from '@material-ui/core/Radio'
 
 export default function PhoneScreen({ state, setState }) {
-  const { isPhoneLandscape, isPhonePortrait } = state
+  const { isPhoneLandscape, storesApi } = state
 
   const [drawerContent, setDrawerContent] = useState()
   const [expanded, setExpanded] = useState(false)
@@ -35,6 +36,19 @@ export default function PhoneScreen({ state, setState }) {
       width: isPhoneLandscape ? '70vw' : '100vw',
       height: isPhoneLandscape ? '100vh' : '70vh',
     },
+    stores_container: {
+      height: '100%',
+    },
+    store_container: {
+      padding: '0 10px',
+    },
+    store_container2: {
+      background: 'rgba(0,0,0,0.2)',
+      borderRadius: '5px',
+    },
+    radio: {
+      color: '#dbdbdb',
+    },
   }
 
   function handleClick(value) {
@@ -43,42 +57,82 @@ export default function PhoneScreen({ state, setState }) {
   }
 
   const Stores = () => {
-    return <p>stores</p>
+    const stores = storesApi.data.map((store, index) => {
+      return (
+        <Grid item style={style.store_container}>
+          <Grid
+            style={style.store_container2}
+            direction={isPhoneLandscape ? 'row' : 'column'}
+            container
+            alignItems="center">
+            <Grid item>
+              <Image
+                src={`https://www.cheapshark.com${store.images.logo}`}
+                layout="fixed"
+                height={30}
+                width={30}
+              />
+            </Grid>
+            <Grid item>
+              <Radio style={style.radio} size="small" />
+            </Grid>
+          </Grid>
+        </Grid>
+      )
+    })
+
+    return (
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        style={style.stores_container}>
+        {stores}
+      </Grid>
+    )
   }
 
   const OrientatedSliders = () => {
-    const orientatedSliders = isPhoneLandscape ? (
-      <Grid
-        container
-        direction="row"
-        className={styles.drawer_container}
-        justifyContent="center"
-        alignItems="center">
-        <Grid item style={{ paddingRight: '50px' }}>
-          <Grid container justifyContent="center">
+    const Landscape = () => {
+      return (
+        <Grid
+          container
+          direction="row"
+          className={styles.drawer_container}
+          justifyContent="center"
+          alignItems="center">
+          <Grid item style={{ paddingRight: '50px' }}>
+            <Grid container justifyContent="center">
+              <Button variant="contained">OK</Button>
+            </Grid>
+          </Grid>
+
+          <Grid item>
+            <Sliders state={state} setState={setState} />
+          </Grid>
+        </Grid>
+      )
+    }
+
+    const Portrait = () => {
+      return (
+        <Grid
+          container
+          direction="column"
+          className={styles.drawer_container}
+          justifyContent="center"
+          alignItems="center">
+          <Grid item>
+            <Sliders state={state} setState={setState} />
+          </Grid>
+          <Grid item>
             <Button variant="contained">OK</Button>
           </Grid>
         </Grid>
+      )
+    }
 
-        <Grid item>
-          <Sliders state={state} setState={setState} />
-        </Grid>
-      </Grid>
-    ) : (
-      <Grid
-        container
-        direction="column"
-        className={styles.drawer_container}
-        justifyContent="center"
-        alignItems="center">
-        <Grid item>
-          <Sliders state={state} setState={setState} />
-        </Grid>
-        <Grid item>
-          <Button variant="contained">OK</Button>
-        </Grid>
-      </Grid>
-    )
+    const orientatedSliders = isPhoneLandscape ? <Landscape /> : <Portrait />
     return orientatedSliders
   }
 
@@ -91,10 +145,10 @@ export default function PhoneScreen({ state, setState }) {
         justifyContent="center"
         alignItems="center"></Grid>
       <div container className={styles.background}>
-        <Image
+        {/* <Image
           src={isPhoneLandscape ? headerBackground : drawerBackground}
           layout="fill"
-        />
+        /> */}
       </div>
       <Grid
         container
@@ -124,9 +178,9 @@ export default function PhoneScreen({ state, setState }) {
           justifyContent="center"
           alignItems="center">
           <Grid item className={styles.drawer} style={style.drawer} xs={12}>
-            <div container className={styles.drawer_background}>
+            {/* <div container className={styles.drawer_background}>
               <Image src={drawerBackground} layout="fill" />
-            </div>
+            </div> */}
             {drawerContent === 'stores' ? <Stores /> : <OrientatedSliders />}
           </Grid>
         </Grid>
